2d7529ccac239201a305003c021b8cee5c0d1de8
Nukr <nukrs.w@gmail.com>
2015-05-26 18:20:07 +0800

    first commit

---
+++ parser.js
@@ -0,0 +1,6 @@
+import protodef from './protodef';
+
+let reql = '[1,[39,[[15,[[14,["blog"]],"users"]],{"name":"Michel"}]],{"binary_format":"raw","time_format":"raw","profile":false}]';
+
+console.log(protodef.VersionDummy);
+
