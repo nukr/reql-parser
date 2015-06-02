02a5b7d1e9cb9269bd28456b8263a07d1f3804ab
Nukr <nukrs.w@gmail.com>
2015-06-02 18:10:41 +0800

    FUNC

--- src/query.js
+++ src/query.js
@@ -28,6 +28,14 @@ class Query {
         return this.filter(term[1])
       case termTypes.FUNC: // 69
         return this.func(term[1])
+      case termTypes.MAKE_ARRAY: // 2
+        return this.makeArray(term[1])
+      case termTypes.VAR: // 10
+        return this.varId(term[1])
+      case termTypes.EQ: // 17
+        return this.eq(term[1])
+      case termTypes.BRACKET: // 170
+        return this.bracket(term[1])
       default:
     }
   }
@@ -55,11 +63,33 @@ class Query {
   }
 
   func (args) {
-    let fnArgs = this.evaluate(args[0])
-    let body = this.evaluate(args[1])
-    return (p) => {
-      return p('id').eq('0006e572-2f24-4d79-87c3-ea6213caba06')
-    }
+    this.fnArgs = this.evaluate(args[0])
+    this.funcBody = args[1]
+    return new Function(
+      this.fnArgs.join(','),
+      `
+        this.runtimeVar = ${this.fnArgs[0]};
+        return this.evaluate(this.funcBody);
+      `
+    ).bind(this)
+  }
+
+  makeArray (args) {
+    return args.map((arg) => `var${arg}`)
+  }
+
+  eq (args) {
+    let sequence = this.evaluate(args[0])
+    return sequence.eq(args[1])
+  }
+
+  bracket (args) {
+    let sequence = this.evaluate(args[0])
+    return sequence(args[1])
+  }
+
+  varId (args) {
+    return this.runtimeVar
   }
 }
 
--- test/query.js
+++ test/query.js
@@ -20,7 +20,7 @@ describe('build query from array', () => {
   })
 
   it("r.db('test').table('bills').filter((b) => {b('id').eq('0006e572-2f24-4d79-87c3-ea6213caba06')})", () => {
-    let query = new Query([ 39, [ [ 15, [ [ 14, ['test'] ], 'bills' ] ], [ 69, [ [ 2, [86] ], [ 17, [ [ 170, [ [ 10, [86] ], 'credit' ] ], true]]]]]])
+    let query = new Query([ 39, [ [ 15, [ [ 14, ['test'] ], 'bills' ] ], [ 69, [ [ 2, [86] ], [ 17, [ [ 170, [ [ 10, [86] ], 'id' ] ], '0006e572-2f24-4d79-87c3-ea6213caba06']]]]]])
     let promise = query.run()
     promise.then((result) => {
       expect(result[0].id).to.be.equal('0006e572-2f24-4d79-87c3-ea6213caba06')
