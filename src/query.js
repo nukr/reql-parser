e14dc62ca7ba16e522793ec1605c4084dd036430
Nukr <nukrs.w@gmail.com>
2015-06-04 16:25:02 +0800

    add some func

--- src/query.js
+++ src/query.js
@@ -25,7 +25,9 @@ class Query {
   evaluate (term, internalOptions) {
     internalOptions = internalOptions || {}
 
-    if(!Array.isArray(term)) return term
+    if(!Array.isArray(term)) {
+      return r.expr(term)
+    }
 
     let termType = term[0]
     switch (termType) {
@@ -41,8 +43,6 @@ class Query {
         return this.makeArray(term[1])
       case termTypes.VAR: // 10
         return this.varId(term[1])
-      case termTypes.EQ: // 17
-        return this.eq(term[1])
       case termTypes.BRACKET: // 170
         return this.bracket(term[1])
       case termTypes.REDUCE: // 37
@@ -51,10 +51,58 @@ class Query {
         return this.map(term[1])
       case termTypes.ADD: // 24
         return this.add(term[1])
+      case termTypes.SUB:
+        return this.sub(term[1])
+      case termTypes.MUL:
+        return this.mul(term[1])
+      case termTypes.DIV:
+        return this.div(term[1])
+      case termTypes.MOD:
+        return this.mod(term[1])
       case termTypes.LIMIT: // 71
         return this.limit(term[1])
       case termTypes.WITH_FIELDS: // 96
         return this.withFields(term[1])
+      case termTypes.COUNT:
+        return this.count(term[1])
+      case termTypes.AND:
+        return this.and(term[1])
+      case termTypes.OR:
+        return this.or(term[1])
+      case termTypes.EQ:
+        return this.eq(term[1])
+      case termTypes.NE:
+        return this.ne(term[1])
+      case termTypes.GT:
+        return this.gt(term[1])
+      case termTypes.GE:
+        return this.ge(term[1])
+      case termTypes.LT:
+        return this.lt(term[1])
+      case termTypes.LE:
+        return this.le(term[1])
+      case termTypes.NOT:
+        return this.not(term[1])
+      case termTypes.RANDOM:
+        return this.random(term[1])
+      case termTypes.NOW:
+        return this.now(term[1])
+      case termTypes.TIME:
+        return this.time(term[1])
+      case termTypes.EPOCH_TIME:
+        return this.epochTime(term[1])
+      case termTypes.ISO8601:
+        return this.ISO8601(term[1])
+      case termTypes.IN_TIMEZONE:
+        return this.inTimezone(term[1])
+      case termTypes.TIMEZONE:
+        return this.timezone(term[1])
+      case termTypes.DURING:
+        return this.during(term[1])
+      case termTypes.HOURS:
+        return this.hours(term[1])
+      case termTypes.DATE:
+        return this.date(term[1])
       default:
         throw new Error.ReqlRuntimeError("Unknown term")
     }
@@ -103,7 +151,44 @@ class Query {
     let var1 = this.evaluate(args[0])
     let var2 = this.evaluate(args[1])
     log('add()', var1, var2)
-    return var1.add(var2);
+    return var1.add(var2)
+  }
+
+  sub (args) {
+    let var1 = this.evaluate(args[0])
+    let var2 = this.evaluate(args[1])
+    log('sub()', var1, var2)
+    return var1.sub(var2)
+  }
+
+  mul (args) {
+    let var1 = this.evaluate(args[0])
+    let var2 = this.evaluate(args[1])
+    log('mul()', var1, var2)
+    return var1.mul(var2)
+  }
+
+  div (args) {
+    let var1 = this.evaluate(args[0])
+    let var2 = this.evaluate(args[1])
+    log('div()', var1, var2)
+    return var1.div(var2)
+  }
+
+  mod (args) {
+    let var1 = this.evaluate(args[0])
+    let var2 = this.evaluate(args[1])
+    log('mod()', var1, var2)
+    return var1.mod(var2)
+  }
+
+  expr (args) {
+    return r.expr(args)
+  }
+
+  count (args) {
+    let sequence = this.evaluate(args[0])
+    return sequence.count()
   }
 
   func (args) {
@@ -128,12 +213,6 @@ class Query {
     return args.map((arg) => `var_${arg}`)
   }
 
-  eq (args) {
-    let sequence = this.evaluate(args[0])
-    log(`.eq(${args[1]})`)
-    return sequence.eq(args[1])
-  }
-
   bracket (args) {
     let sequence = this.evaluate(args[0])
     log(`.(${args[1]})`)
@@ -155,6 +234,100 @@ class Query {
     return sequence.withFields(...args)
   }
 
+  // Math and logic
+  and (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.and(...args)
+  }
+
+  or (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.or(...args)
+  }
+
+  eq (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.eq(...args)
+  }
+
+  ne (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.ne(...args)
+  }
+
+  gt (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.gt(...args)
+  }
+
+  ge (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.ge(...args)
+  }
+
+  lt (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.lt(...args)
+  }
+
+  le (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.le(...args)
+  }
+
+  not (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.not()
+  }
+
+  random (args) {
+    return r.random(...args)
+  }
+
+  // Dates and times
+
+  now () {
+    return r.now()
+  }
+
+  time (args) {
+    return r.time(...args)
+  }
+
+  epochTime(args) {
+    return r.epochTime(...args)
+  }
+
+  ISO8601(args) {
+    return r.ISO8601(...args)
+  }
+
+  inTimezone(args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.inTimezone(...args)
+  }
+
+  timezone(args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.timezone(...args)
+  }
+
+  during(args) {
+    let var1 = this.evaluate(args.shift())
+    let var2 = this.evaluate(args.shift())
+    let var3 = this.evaluate(args.shift())
+    return var1.during(var2, var3)
+  }
+
+  date (args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.date()
+  }
+
+  hours(args) {
+    let var1 = this.evaluate(args.shift())
+    return var1.hours()
+  }
 }
 
 export default Query
